65099c0f573d8e82562bcb651a1e03aa7cfb354a
moakdoge <moakdoge@gmail.com>
2026-07-22 14:04:18 -0700

    fix - recache

--- fyenid/types/core.py
+++
@@ -1,8 +0,0 @@
-from typing import TypeAlias
-
-
-MessageID: TypeAlias = int
-AttachmentID: TypeAlias = int
-UserID: TypeAlias = int
-UnixTime: TypeAlias = int
-ChannelID: TypeAlias = int
--- fyenid/types/json_types/user.py
+++
@@ -1,5 +0,0 @@
-#chat.users.get_user(id=user_id).username
-from pydantic import BaseModel, Field
-from ..core import *
-class UserObject(BaseModel):
-    username: str
